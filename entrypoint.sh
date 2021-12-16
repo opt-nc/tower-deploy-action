339b61b07378800331f2a06572bc14d55da24e1a
Michèle BARRE <michele.barre@opt.nc>
2021-12-16 13:31:10 +1100

    build: add message after job

--- entrypoint.sh
+++ entrypoint.sh
@@ -21,3 +21,13 @@ export TOWER_TIMEOUT=$8
 
 /prepare_extra_vars.py
 /launch_job.py
+
+result=$?
+if [ $result -ne 0 ] ; then
+  echo "ERROR : Deployment failed." ;
+  echo "Check you changes and relaunch, or try to deploy manually" ;
+else
+  echo "INFO : Automatic deployment succeeded." ;
+fi ;
+
+exit $result
