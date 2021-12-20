a345b6046e558887e3b3917f4d3ed70f8c140808
Daniel Santos <dougniel@gmail.com>
2021-12-20 19:34:57 +1100

    fix(params): using env variables to try to workaround params encoding issue

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,26 +1,4 @@
 #!/bin/sh -l
-
-# $1 extravars_template_filename
-# $2 vars
-# $3 asset_url
-# $4 tower_template_id
-# $5 tower_url
-# $6 tower_user
-# $7 tower_password
-# $8 tower_timeout
-
-export EXTRA_VARS_TEMPLATE_FILENAME=$1
-export ARTIFACT_URL=$3
-export SECRETS_CONTEXT="$2" 
-export TOWER_TEMPLATE_ID=$4
-export EXTRA_VARS_FILE="/github/workspace/tmp_extra_vars.txt"
-export TOWER_URL=$5
-export TOWER_USER=$6
-export TOWER_PASSWORD=$7
-export TOWER_TIMEOUT=$8
-
-echo "$2" | jq keys
-
 /prepare_extra_vars.py
 /launch_job.py
 
