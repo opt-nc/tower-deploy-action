f60163ba92845f3b981c693b2220776654981825
Daniel Santos <dougniel@gmail.com>
2021-12-20 17:11:00 +1100

    fix(secrets): double-quote in secret value error

--- entrypoint.sh
+++ entrypoint.sh
@@ -11,13 +11,16 @@
 
 export EXTRA_VARS_TEMPLATE_FILENAME="/github/workspace/app/src/main/resources/"$1
 export ARTIFACT_URL=$3
-export SECRETS_CONTEXT=$2
+export SECRETS_CONTEXT=$2 
 export TOWER_TEMPLATE_ID=$4
 export EXTRA_VARS_FILE="/github/workspace/tmp_extra_vars.txt"
 export TOWER_URL=$5
 export TOWER_USER=$6
 export TOWER_PASSWORD=$7
 export TOWER_TIMEOUT=$8
+
+# fix double quote secret value
+SECRETS_CONTEXT=$(echo $SECRETS_CONTEXT | sed -s 's,\\\\",\\\",g')
 
 /prepare_extra_vars.py
 /launch_job.py
