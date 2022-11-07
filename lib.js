cab616c7dcf4b702c969c6bd97b0b5452e054e15
Daniel Santos <39691264+Dougniel@users.noreply.github.com>
2022-11-07 15:52:31 +0100

    refactor: Portage Docker/Python vers js  ⏱ (#31)
    * refactor: Portage Docker/Python vers js  ⏱
    
    - Expressions `$expr` non évaluées
    - Fix url de status
    
    fix #13

--- lib.js
+++ lib.js
@@ -15,13 +15,17 @@ module.exports = async function () {
       GITHUB_RUN_ID: process.env.GITHUB_RUN_ID, // Tower workaround to force restart
     };
     const varsFilename = core.getInput('extravars_template_filename');
-    const vars = fs.readFileSync(varsFilename).toString().replace('$(\\W)', v => (v in values ? values[v] : v));
-    core.info(`⚙️ EXTRA_VARS file evaluated :\n ${vars}`);
+    const vars = fs
+      .readFileSync(varsFilename)
+      .toString()
+      .replace(/\$(\w+)/g, (m, p1) => (p1 in values ? values[p1] : p1));
 
     // launch Tower by a rest API
     const towerTemplateId = core.getInput('tower_template_id');
     const towerUrl = core.getInput('tower_url');
     const auth = { username: core.getInput('tower_user'), password: core.getInput('tower_password') };
+
+    core.info(`⚡️ Launching Tower job ${towerUrl}/job_templates/${towerTemplateId}/launch :\n${vars}`);
 
     const response = await axios({
       method: 'POST',
@@ -38,8 +42,8 @@ module.exports = async function () {
     let step = 0;
     while (step < maxsteps) {
       await setTimeout(WAITSTEP * 1000);
-      const res = await axios({ url: `${towerUrl}/jobs/${jobId}`, auth });
-      
+      const res = await axios({ url: `${towerUrl}/jobs/${jobId}/`, auth });
+
       if (res.data.status === 'successful') {
         break;
       } else if (res.data.status === 'failed' || res.data.status === 'cancelled') {
@@ -52,7 +56,7 @@ module.exports = async function () {
       step++;
     }
 
-    core.info(`✅ Automatic deployment succeeded.`)
+    core.info(`✅ Automatic deployment succeeded.`);
   } catch (error) {
     if (error.response && error.response.status === 401) {
       core.setFailed('HTTP 401 : maybe you have to check tower_ser/tower_password inputs ?');
